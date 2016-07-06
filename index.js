f75206f6d3bbcc17bd8a6ba2865d442dff9a3675
Heather Park <hthr.prk@gmail.com>
2016-07-06 15:34:44 -0700

    First commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('./server.js');
 
- var port = 4568;
+var port = 4568;
 
-app.listen(port);
+app.listen(process.env.PORT || port);
 
 console.log('Server now listening on port ' + port);
